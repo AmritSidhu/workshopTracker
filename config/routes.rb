50b981754658a3e9ac642ed86e9351d4280b7572
Amrit Sidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2018-08-23 11:44:34 +0100

    Fetching a list of calendars Fetching calendar events Adding an event

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/redirect', to: 'workshop#redirect', as: 'redirect'
   get '/callback', to: 'workshop#callback', as: 'callback'
-  get '/calendars', to: 'example#calendars', as: 'calendars'
+  get '/calendars', to: 'workshop#calendars', as: 'calendars'
+  get '/events/:calendar_id', to: 'workshop#events', as: 'events', calendar_id: /[^\/]+/
+  post '/events/:calendar_id', to: 'workshop#new_event', as: 'new_event', calendar_id: /[^\/]+/
 end
