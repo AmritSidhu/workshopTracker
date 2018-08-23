18e7ef89ff4ac66ec538ac3c60786f537f091dac
Amrit Sidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2018-08-23 11:27:46 +0100

    Installing the google-api-client gem Authorizing access to Google Calendar Testing the authorization redirect Fetching an access token

---
+++ app/controllers/workshop_controller.rb
@@ -0,0 +1,33 @@
+require 'signet/oauth_2/client'
+require 'google/apis/calendar_v3'
+
+class WorkshopController < ApplicationController
+    def redirect
+        client = Signet::OAuth2::Client.new(client_options)
+        redirect_to client.authorization_uri.to_s
+    end
+    
+    def callback
+        client = Signet::OAuth2::Client.new(client_options)
+        client.code = params[:code]
+    
+        response = client.fetch_access_token!
+    
+        session[:authorization] = response
+    
+        redirect_to calendars_url
+      end
+    
+      private
+    
+      def client_options
+        {
+          client_id: Rails.application.secrets.google_client_id,
+          client_secret: Rails.application.secrets.google_client_secret,
+          authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+          token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+          scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
+          redirect_uri: callback_url
+        }
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/redirect', to: 'workshop#redirect', as: 'redirect'
+  get '/callback', to: 'workshop#callback', as: 'callback'
+  get '/calendars', to: 'example#calendars', as: 'calendars'
 end
